d3e962024b786992f32ef7f01565d1e6dd8aa8f8
FebryanDWiCahyo <febrydwicahyo1@gmail.com>
2023-10-31 19:46:56 +0700

    Modul3

---
+++ AutocompleteAndTemplates.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+public class AutocompleteAndTemplates {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        out.println("Kalkulator Sederhana");
+        out.print("Masukkan angka pertama: ");
+        double angka1 = input.nextDouble();
+
+        out.print("Masukkan operasi (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        out.print("Masukkan angka kedua: ");
+        double angka2 = input.nextDouble();
+
+        double hasil;
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                break;
+            case '/':
+                if (angka2 != 0) {
+                    hasil = angka1 / angka2;
+                } else {
+                    out.println("Pembagian oleh nol tidak valid.");
+                    return;
+                }
+                break;
+            default:
+                out.println("Operasi matematika tidak valid.");
+                return;
+        }
+
+        out.println("Hasil: " + hasil);
+
+        input.close();
+        case '/':
+        if (angka2 != 0) {
+            hasil = angka1 / angka2;
+        } else {
+            out.println("Pembagian oleh nol tidak valid.");
+            return;
+
+        }
+
+
+}
---
+++ MenghitungLuasLingkaran.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Program ini menghitung luas lingkaran berdasarkan jari-jari yang dimasukkan oleh pengguna.
+ */
+public class MenghitungLuasLingkaran {
+    public static void main(String[] args) {
+        // Membuat objek Scanner untuk masukan pengguna
+        Scanner input = new Scanner(System.in);
+
+        // Meminta pengguna memasukkan jari-jari lingkaran
+        System.out.print("Masukkan jari-jari lingkaran: ");
+        /**
+         *  r = masukan inputannya
+         */
+        double r = input.nextDouble();
+        /**
+         *
+         */
+        // Menghitung luas lingkaran
+        double luas = Math.PI * r * r;
+
+        // Menampilkan hasil
+        System.out.println("Luas lingkaran dengan jari-jari " + r + " adalah: " + luas);
+
+        // Menutup Scanner untuk mencegah kebocoran sumber daya
+        input.close();
+    }
+}
